040fbc41baa93dd292a48e30590571ac9a144f44
test2slam <test2slam@jolsio.net@P-19189-F401.jolsio.net>
2020-10-16 14:39:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lactemprature/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lactemprature;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button saisirreleve = findViewById(R.id.saisirReleve);
+        Button afficherreleve = findViewById(R.id.afficherReleve);
+        Button moyennereleve = findViewById(R.id.moyenneReleve);
+        View.OnClickListener ecouteur = new View.OnClickListener() {
+
+
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.saisirReleve:
+                        Intent intent1 = new Intent(MainActivity.this, saisirreleve.class);
+                        startActivity(intent1);
+                        break;
+                    case R.id.afficherReleve:
+                        Intent intent2 = new Intent(MainActivity.this, afficherreleve.class);
+                        startActivity(intent2);//on affecte au bouton l'écouteurbtnNewReleve.setOnClickListener(ecouteur);btnListeClients.setOnClickListener(ecouteur);
+                    case R.id.moyenneReleve:
+                        Intent intent3 = new Intent( MainActivity.this, moyennereleve.class);
+                        startActivity(intent3);
+                        break;
+
+                };
+    }
